ec0e3a988a73f6b4d268acfdaa9582b5f6b94d76
maruands <maruands@gmail.com>
2024-01-03 10:51:26 +0300

    multiply, random, mod and max

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2 ;
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+// calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+var num3 = 19;
+var num4 = 5;
+var mod = num3 % num4
+
+// finds the highest number in a set; the value returned should be 20
+var set = (12,10,9,7,20);
+var max = Math.max(set);
